673f2e6bcb5821a6347b3cdd1ec66f8028054566
jared83 <jared.t83@gmail.com>
2017-11-27 02:18:15 +0000

    Dealing with duplicates

--- public/script.js
+++ public/script.js
@@ -1,1 +1,32 @@
-console.log('It works.');
+var app = new Vue({
+    el: '#app',
+    data: {
+        total: 2,
+        items: [
+            { title: 'Item1', quantity: 1},
+            { title: 'Item2', quantity: 1},
+            { title: 'Item3', quantity: 1},
+        ],
+        cart: []
+    },
+    methods: {
+        addItem: function(index){
+            
+            if (this.cart.length > 0) {
+                added = false;
+                this.cart.forEach(el => {
+                    if (this.items[index].title === el.title) {
+                        el.quantity += 1;
+                        added = true;
+                    } 
+                });
+                if (!added) {
+                    this.cart.push(this.items[index]);
+                }
+            }
+            else 
+                this.cart.push(this.items[index]);
+            this.total += 9.99;
+        }
+    }
+})
