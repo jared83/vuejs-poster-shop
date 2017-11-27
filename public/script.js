2657f6abe140cf670ac2f1ba61de82385a63b893
jared83 <jared.t83@gmail.com>
2017-11-27 02:19:04 +0000

    Inc + Dec Items in the cart.

--- public/script.js
+++ public/script.js
@@ -1,32 +1,49 @@
 var app = new Vue({
     el: '#app',
     data: {
-        total: 2,
+        total: 0,
         items: [
-            { title: 'Item1', quantity: 1},
-            { title: 'Item2', quantity: 1},
-            { title: 'Item3', quantity: 1},
+            { title: 'Item1', quantity: 0, price: 9.99},
+            { title: 'Item2', quantity: 0, price: 9.99},
+            { title: 'Item3', quantity: 0, price: 9.99},
+            { title: 'Item3', quantity: 0, price: 9.99}
         ],
         cart: []
     },
     methods: {
         addItem: function(index){
-            
-            if (this.cart.length > 0) {
-                added = false;
-                this.cart.forEach(el => {
-                    if (this.items[index].title === el.title) {
-                        el.quantity += 1;
-                        added = true;
-                    } 
-                });
-                if (!added) {
-                    this.cart.push(this.items[index]);
-                }
-            }
-            else 
+            var item = this.items[index];
+            item.quantity ? item.quantity : item.quantity = 1;
+            added = false;
+            this.cart.forEach(el => {
+                if (this.items[index] === el) {
+                    el.quantity += 1;
+                    added = true;
+                } 
+            });
+            if (!added) {
                 this.cart.push(this.items[index]);
-            this.total += 9.99;
+            }
+            this.total += item.price;
+        },
+        inc: function(item){
+            item.quantity++;
+            this.total += item.price;
+        },
+        dec: function(item){
+            if (item.quantity > 1) {
+                item.quantity--;
+                this.total -= item.price;
+            }else {
+                let index = this.cart.indexOf(item);
+                item.quantity = 0;
+                this.cart.splice(index, 1);
+            }
+        }
+    }, 
+    filters: {
+        currency: function(price) {
+            return '$'.concat(price.toFixed(2));
         }
     }
 })
