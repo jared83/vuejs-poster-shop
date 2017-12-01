6ef9865bf0b2199d38977f38c08323b39be12e9e
jared83 <jared.t83@gmail.com>
2017-12-01 01:34:14 +0000

    Server-side commented 1

--- index.js
+++ index.js
@@ -8,24 +8,24 @@ var querystring = require('querystring');
 require('dotenv').config();
 
 var bodyParser = require('body-parser');
-app.use( bodyParser.json() );
+app.use( bodyParser.json() ); //Parses the body of a RESTful response to json
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/index.html')); //Sends index.html to the client.
 });
 
-var instance = axios.create({
-  baseURL: 'https://api.imgur.com/3/',
-  headers: { 'Authorization': 'Client-ID ' + process.env.IMGUR_CLIENT_ID }
+var instance = axios.create({ // axios instance used to send an (autthencitated) html request. Imgur in this case
+  baseURL: 'https://api.imgur.com/3/', //what and where from?
+  headers: { 'Authorization': 'Client-ID ' + process.env.IMGUR_CLIENT_ID } // Auth data
 });
 
-app.get('/search/:query', function(req, res) {
-  const url = 'gallery/search/top/0/?' + querystring.stringify({ q: req.params.query });
-  instance.get(url)
-    .then(function (result) {
-      res.send(result.data.data.filter(item => !item.is_album && !item.nsfw && !item.animated));
+app.get('/search/:query', function(req, res) { // an end point from where the client will access the images' data returned from Imgur.
+  const url = 'gallery/search/top/0/?' + querystring.stringify({ q: req.params.query }); // build url with query sent to imgur...
+  instance.get(url) // sending the query to imgur
+    .then(function (result) { // executes when querried validly...
+      res.send(result.data.data.filter(item => !item.is_album && !item.nsfw && !item.animated)); // sending the data to the client while filtering out things we do not need.
     })
-    .catch(function (error) {
+    .catch(function (error) { // executes when error occurred.
       console.log(error);
     })
   ;
@@ -34,10 +34,10 @@ app.get('/search/:query', function(req, res) {
 app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-if (process.env.NODE_ENV !== 'production') {
-  require('reload')(server, app);
+if (process.env.NODE_ENV !== 'production') { // setting up server for production.
+  require('reload')(server, app); // live reload when in poduction.
 }
 
-server.listen(process.env.PORT, function () {
+server.listen(process.env.PORT, function () { // listening to requests on a defined port.
   console.log('Listening on port '.concat(process.env.PORT))
 });
